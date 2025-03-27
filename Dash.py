865f3e8b23e3d194f5e81b8e16d227aa2780793c
joshuamorell <55568726+joshuamorell@users.noreply.github.com>
2025-03-27 11:03:06 -0600

    Dash.py

--- Dash.py
+++ Dash.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, State
 import pandas as pd
 import plotly.express as px
 import os
@@ -87,13 +87,14 @@ def update_plot(site, compound):
 @app.callback(
     Output("download-dataframe-csv", "data"),
     Input("download-btn", "n_clicks"),
-    Input("site-dropdown", "value"),
-    Input("compound-dropdown", "value"),
+    State("site-dropdown", "value"),
+    State("compound-dropdown", "value"),
     prevent_initial_call=True
 )
 def download_data(n_clicks, site, compound):
-    filtered_df = df[(df['Site'] == site) & (df['Compound'] == compound)]
-    return dcc.send_data_frame(filtered_df.to_csv, "filtered_data.csv")
+    if n_clicks > 0:  # Only trigger when the button is clicked
+        filtered_df = df[(df['Site'] == site) & (df['Compound'] == compound)]
+        return dcc.send_data_frame(filtered_df.to_csv, "filtered_data.csv")
 
 
 # Export server for Gunicorn
